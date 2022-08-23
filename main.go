c0ed2ce3b897606110f7906abdd30617752c64ac
Fredrik Enestad <fredrik@enestad.com>
2022-08-23 17:04:46 +0200

    add a catch all handler with some logging

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 
 func main() {
 	http.HandleFunc("/healthz", healthz)
+	http.HandleFunc("/", any)
 	http.ListenAndServe(":3000", nil)
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+func any(w http.ResponseWriter, r *http.Request) {
+	println(r.URL.Path)
 	w.Write([]byte("OK"))
 }
